7a33d0de7cb640c82df8a32bb34c76a78af0e9e5
Nicolas Pernoud <github@ninico.fr>
2021-03-01 14:48:01 +0100

    feat: added gauge for bmi

--- lib/screens/journey.dart
+++ lib/screens/journey.dart
@@ -4,6 +4,7 @@ import 'package:princess_journey/components/water_intakes_chart.dart';
 import 'package:princess_journey/components/weight_chart.dart';
 import 'package:princess_journey/models/user.dart';
 import 'package:provider/provider.dart';
+import 'package:customgauge/customgauge.dart';
 
 import '../i18n.dart';
 
@@ -38,10 +39,42 @@ class _JourneyState extends State<Journey> {
                       ListTile(
                         leading: Icon(Icons.pie_chart),
                         title: Text(MyLocalizations.of(context).tr("your_bmi")),
-                        subtitle: Consumer<User>(
-                            builder: (context, user, child) =>
-                                Text(user.bmi.toString())),
                       ),
+                      Consumer<User>(
+                          builder: (context, user, child) => CustomGauge(
+                                showMarkers: false,
+                                gaugeSize: 100,
+                                minValue: 0,
+                                maxValue: 45,
+                                segments: [
+                                  GaugeSegment('Very severely underweight ', 15,
+                                      Colors.red),
+                                  GaugeSegment('Severely underweight ', 1,
+                                      Colors.orange),
+                                  GaugeSegment(
+                                      'Underweight ', 2.5, Colors.yellow),
+                                  GaugeSegment('Normal (healthy weight) ', 6.5,
+                                      Colors.green),
+                                  GaugeSegment('Overweight ', 5, Colors.yellow),
+                                  GaugeSegment(
+                                      'Obese Class I (Moderately obese) ',
+                                      5,
+                                      Colors.orange),
+                                  GaugeSegment(
+                                      'Obese Class II (Severely obese) ',
+                                      5,
+                                      Colors.red),
+                                  GaugeSegment(
+                                      'Obese Class III (Very severely obese) ',
+                                      5,
+                                      Colors.red),
+                                ],
+                                currentValue: user.bmi,
+                                valueWidget: Text(user.bmi.toString(),
+                                    style: TextStyle(
+                                        fontSize: 12,
+                                        fontWeight: FontWeight.bold)),
+                              )),
                     ]))),
         Card(
             child: Padding(
